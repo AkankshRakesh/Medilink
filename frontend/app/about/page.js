1679ecc07b2afab2f52d77e54dd0d31f24fd3702
AkankshRakesh <akankshrakesh@gmail.com>
2025-03-19 14:14:26 +0530

    about us page

---
+++ frontend/app/about/page.js
@@ -0,0 +1,15 @@
+import { Aboutus } from "@/components/Aboutus";
+import Main from "@/components/Main";
+
+export const metadata = {
+    title: "Medilink · Contact Us",
+};
+
+export default function ContactPage() {
+    
+    return (
+        <Main>
+            <Aboutus/>
+        </Main>
+    )
+}
