d47e21758931cb0c46cb35eca67fd3d2e41cfe49
AkankshRakesh <akankshrakesh@gmail.com>
2025-03-26 19:14:57 +0530

    Suspense fallback

--- frontend/app/booking/page.js
+++ frontend/app/booking/page.js
@@ -1,6 +1,7 @@
 
 import BookingPage from "@/components/BookingPage";
 import Main from "@/components/Main";
+import { Suspense } from "react";
 
 export const metadata = {
     title: "Medilink · Booking",
@@ -10,7 +11,9 @@ export default function ContactPage() {
     
     return (
         <Main>
+            <Suspense fallback={<div>Loading...</div>}>
             <BookingPage/>
+            </Suspense>
         </Main>
     )
 }
