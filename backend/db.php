fd0eda0b941e8a78f58e4f6e020e56b2ee3ab7aa
AkankshRakesh <akankshrakesh@gmail.com>
2025-04-01 22:10:50 +0530

    YEE

--- backend/db.php
+++ backend/db.php
@@ -1,11 +1,10 @@
 <?php
-$host = 'localhost';
-$dbname = 'moodl';
-$username = 'root'; 
-$password = ''; 
+$host = 'sql12.freesqldatabase.com';
+$dbname = 'sql12770743';
+$username = 'sql12770743'; 
+$password = getenv('PASS'); 
 
 try {
-    echo "YEEE";
     $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
