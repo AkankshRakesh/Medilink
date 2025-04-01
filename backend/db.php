34b8539f0e2bd7575795f87907e1ce29fab8fbc5
AkankshRakesh <akankshrakesh@gmail.com>
2025-04-01 20:16:15 +0530

    YEE

--- backend/db.php
+++ backend/db.php
@@ -5,6 +5,7 @@ $username = 'root';
 $password = ''; 
 
 try {
+    echo "YEEE";
     $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
