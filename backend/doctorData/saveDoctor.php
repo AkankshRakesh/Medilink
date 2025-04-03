f04457d82ee7337353ee3a071a129e20ca66467b
AkankshRakesh <akankshrakesh@gmail.com>
2025-04-03 19:10:44 +0530

    save doctor cassadsaddas

--- backend/doctorData/saveDoctor.php
+++ backend/doctorData/saveDoctor.php
@@ -12,9 +12,10 @@ $allowedOrigins = [
     "https://moodl-wheat.vercel.app/"
 ];
 
-if (in_array($_SERVER['HTTP_ORIGIN'], $allowedOrigins)) {
-    header("Access-Control-Allow-Origin: " . $_SERVER['HTTP_ORIGIN']);
-}
+// if (in_array($_SERVER['HTTP_ORIGIN'], $allowedOrigins)) {
+//     header("Access-Control-Allow-Origin: " . $_SERVER['HTTP_ORIGIN']);
+// }
+header("Access-Control-Allow-Origin: " . "https://moodl-wheat.vercel.app/");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type");
 header("Access-Control-Allow-Credentials: true");
